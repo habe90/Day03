df165ce744a64448fb167cc3fb1c126a41fe6ac5
Halil Hodzic <habetech@gmail.com>
2021-03-10 19:52:15 +0100

    sum two number

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //sum two numbers
+    var numOne = 15;
+    var numTwo = 8;
+
+    var sum = numOne + numTwo;
+
+    return [sum];
+}
+
+console.log(main());
+module.exports = main;
