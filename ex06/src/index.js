6a82818188f77d7fbddb54052cca4637dbf72e08
Halil Hodzic <habetech@gmail.com>
2021-03-10 19:34:10 +0100

    unintialized variables

---
+++ ex06/src/index.js
@@ -0,0 +1,14 @@
+
+function main() {
+    var x = 9, 
+        y = 16;
+
+    var z = "My name is John",
+        age = "and i'm",
+        year = "years old";
+
+    return [z,age,x+y,year];
+}
+
+console.log(main());
+module.exports = main;
