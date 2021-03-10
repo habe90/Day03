9730c34ad3be69a1373fff16eed6b8015f79b8ff
Halil Hodzic <habetech@gmail.com>
2021-03-10 19:44:41 +0100

    case sensitive

---
+++ ex07/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+    var studlyCapVar = 10, 
+        properCamelCase = "Its a string",
+        titleCaseOver = 9000;
+
+
+    return [
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+    ];
+}
+
+console.log(main());
+module.exports = main;
