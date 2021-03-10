ff931c83fb37b5ff54258fd07709b7ce0cb5de1d
Halil Hodzic <habetech@gmail.com>
2021-03-10 19:58:03 +0100

    multiplying two number

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //multiplying two numbers
+    var numOne = 15;
+    var numTwo = 3;
+
+    var product = numOne * numTwo;
+
+    return product;
+}
+
+console.log(main());
+module.exports = main;
