5a5138156d48d11dea0934cb2820f21fba81e833
Halil Hodzic <habetech@gmail.com>
2021-03-10 19:56:01 +0100

    substract two number

---
+++ ex09/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    //subtract two numbers
+    var numOne = 15;
+    var numTwo = 8;
+
+    var difference = numOne - numTwo;
+
+    return difference;
+}
+
+console.log(main());
+module.exports = main;
